1d7b951cd487d094451bd88f8ee9f8daeeb0c55c
Elena Brazhnikova <lenovosibirsk@mail.ru>
2022-09-08 12:14:29 +0700

    feat:implement section portfolio

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,7 @@
 import menu from './modules/menu';
+import progress from './modules/_progress-bar';
 
 window.addEventListener('DOMContentLoaded' , () => {
     menu('.hamburger', '.menu__close', '.menu', 'active');
+    progress();
 })
